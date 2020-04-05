9e133ef80dbf9161e52d78cb48278bdd900fec20
Geovani <geovaninogueira02@gmail.com>
2020-04-05 17:47:41 -0300

    Finalizado aulas de serializar e deserealizar arquivos XML

---
+++ Serializar_Deserealizar/02_SerealizarXML/Program.cs
@@ -0,0 +1,21 @@
+﻿using _00_Biblioteca;
+using System;
+using System.IO;
+using System.Xml.Serialization;
+namespace _02_SerealizarXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = System.AppDomain.CurrentDomain.BaseDirectory.ToString();
+            StreamReader reader = new StreamReader(path + "01_SerializarXML.xml");
+
+            XmlSerializer serializador = new XmlSerializer(typeof(Usuario));
+            Usuario usuario = (Usuario)serializador.Deserialize(reader);
+            Console.Write("Nome: {0}, CPF: {1}, Email: {2}", usuario.Nome, usuario.CPF, usuario.Email);
+            Console.ReadKey();
+
+        }
+    }
+}
