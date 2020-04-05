1a41033b0b9442206c69dbbb8954ae488c71b40a
Geovani <geovaninogueira02@gmail.com>
2020-04-05 19:49:06 -0300

    Finalizado Seção Generics

--- Topicos_Avancados/01_Generics/Program.cs
+++ Topicos_Avancados/01_Generics/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _01_Generics.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,38 @@ namespace _01_Generics
     {
         static void Main(string[] args)
         {
+            Carro carro = new Carro()
+            {
+                Marca = "Fiat",
+                Modelo = "UNO"
+            };
+
+            Casa casa = new Casa()
+            {
+                Cidade = "São Paulo",
+                Endereco = "Rua teste nº 55, 08581-190"
+            };
+
+            Usuario usuario = new Usuario()
+            {
+                Nome = "Geovani",
+                Email = "geovaninogueira02@gmail.com",
+                Senha = "12346"
+            };
+
+            Serializador.Serializar(carro);
+            Serializador.Serializar(casa);
+            Serializador.Serializar(usuario);
+
+            Carro carro2 = Serializador.Deserializar<Carro>();
+            Casa casa2 = Serializador.Deserializar<Casa>();
+            Usuario usuario2 = Serializador.Deserializar<Usuario>();
+
+            Console.WriteLine("Carro2: " + carro2.Marca + " - " + carro2.Modelo);
+            Console.WriteLine("Casa2: " + casa2.Cidade + " - " + casa2.Endereco);
+            Console.WriteLine("Usuario2: " + usuario2.Nome + " - " + usuario2.Email + " - " + usuario2.Senha);
+
+            Console.ReadKey();
         }
     }
 }
---
+++ Topicos_Avancados/01_Generics/Serializador.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace _01_Generics
+{
+    public class Serializador
+    {
+        private static string path = System.AppDomain.CurrentDomain.BaseDirectory.ToString();
+
+        public static void Serializar(object obj)
+        {
+            StreamWriter writer = new StreamWriter(path + "03_" + obj.GetType().Name.Trim() + ".txt");
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            String Json = serializer.Serialize(obj);
+            writer.WriteLine(Json);
+            writer.Close();
+
+        }
+
+        public static T Deserializar <T>()
+        {
+            StreamReader reader = new StreamReader(path + "03_" + typeof(T).Name.Trim() + ".txt");
+            String fileReader = reader.ReadToEnd();
+            JavaScriptSerializer serializar = new JavaScriptSerializer();
+            T obj = (T)serializar.Deserialize(fileReader, typeof(T));
+            // object obj = serializar.Deserialize<T>(fileReader);
+            return obj;
+        }
+
+    }
+}
